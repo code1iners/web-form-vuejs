9ecdbaca292d7e9004f44be0aadae7c017d43583
Codeliner <codeliner96@gmail.com>
2021-04-03 10:59:47 +0900

    Added v-model with form

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -2,7 +2,6 @@
   <form>
     <label>Email:</label>
     <input type="email" required v-model="email" />
-
     <label>Password:</label>
     <input type="password" required v-model="password" />
   </form>
